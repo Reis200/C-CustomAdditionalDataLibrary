5706da4a4eb4f125200793cdd4e929b241c53577
Reis200 <hiya8993@gmail.com>
2026-08-22 03:54:47 +0100

    binary search tree design and implementation has changed, also added size and height functions, along with bug fixes to previous functions.

--- include/binarysearchtree.h
+++ include/binarysearchtree.h
@@ -2,12 +2,16 @@
 #define BINARYSEARCHTREE_H
 
 // or binary set tree as no duplicate values allowed in this implementation
-typedef struct BinarySearchTree {
-    void *data;
+typedef struct BinarySearchTreeNode {
+  void *data;
+  struct BinarySearchTreeNode *left;
+  struct BinarySearchTreeNode *right;
+}BinarySearchTreeNode;
 
-    struct BinarySearchTree *left;
-    struct BinarySearchTree *right;
-}BinarySearchTree;
+typedef struct {
+  BinarySearchTreeNode *root;
+  size_t size; // O(1) access much faster
+} BinarySearchTree;
 
 /*
  COMPARISON FUNCTION:
@@ -19,29 +23,31 @@ typedef int (*BSTCompareFunc)(const void *a, const void *b);
 
 
 /*Make / insert*/
-BinarySearchTree* bst_insert(BinarySearchTree *root,void *data,BSTCompareFunc compare);
+BinarySearchTree* bst_insert(BinarySearchTree *tree,void *data,BSTCompareFunc compare);
 
 /*Search*/
-void* bst_find(const BinarySearchTree *root,const void *data,BSTCompareFunc compare);
+void* bst_find(const BinarySearchTree *tree,const void *data,BSTCompareFunc compare);
 
 /*Remove*/
-void* bst_remove(BinarySearchTree *root,const void *data,BSTCompareFunc compare);
+void* bst_remove(BinarySearchTree *tree,const void *data,BSTCompareFunc compare);
 
 
 /*Information*/
-void* bst_size(const BinarySearchTree *root);
 
-void* bst_height(const BinarySearchTree *root);
+// height(null) = 0 / height(leaf) = 1
+int bst_height(const BinarySearchTree *tree);
+
+size_t bst_size(const BinarySearchTree *tree);
 
 /*Traversal*/
 
-void bst_inorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_inorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
-void bst_preorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_preorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
-void bst_postorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_postorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
 /*Cleanup*/
-void bst_destroy(const BinarySearchTree *root, void (*destroyData)(void *data));
+void bst_destroy(const BinarySearchTree *tree, void (*destroyData)(void *data));
 
 #endif
--- src/binarysearchtree.c
+++ src/binarysearchtree.c
@@ -2,21 +2,27 @@
 
 #include <stdio.h>
 
+
 /*Make / insert*/
-BinarySearchTree* bst_insert(BinarySearchTree *root,void *data,BSTCompareFunc compare){
-    if (root == NULL){
-        BinarySearchTree *newRoot = malloc(sizeof(BinarySearchTree));
-        if (newRoot == NULL){
+BinarySearchTree* bst_insert(BinarySearchTree *tree,void *data,BSTCompareFunc compare){
+    if (tree == NULL || compare == NULL){
+        return NULL;
+    }
+    if (tree->root == NULL){
+        BinarySearchTreeNode *newNode = malloc(sizeof(*newNode));
+        if (newNode == NULL){
             return NULL;
         }
-        newRoot->data = data;
-        newRoot->left = NULL;
-        newRoot->right = NULL;
-        return newRoot;
+        newNode->data = data;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        tree->root = newNode;
+        tree->size++;
+        return tree;
     }
 
-    BinarySearchTree *currentRoot = root;
-    BinarySearchTree *parentRoot = NULL;
+    BinarySearchTreeNode *currentRoot = tree->root;
+    BinarySearchTreeNode *parentRoot = NULL;
     int comparison = 0;
     while (currentRoot != NULL){
         parentRoot = currentRoot;
@@ -29,44 +35,48 @@ BinarySearchTree* bst_insert(BinarySearchTree *root,void *data,BSTCompareFunc co
             currentRoot = currentRoot->right;
         }else {
             // duplicate policy: no duplicate allowed (set like tree) 
-            break; 
+            return tree; 
         }
     }
-    BinarySearchTree *newRoot = malloc(sizeof(BinarySearchTree));
+    BinarySearchTreeNode *newNode = malloc(sizeof(*newNode));
 
-    if (newRoot == NULL){
+    if (newNode == NULL){
         return NULL;
     }
 
-    newRoot->data = data;
-    newRoot->left = NULL;
-    newRoot->right = NULL;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
     if (comparison < 0){
         // assign it to left tree
-        parentRoot->left = newRoot;
+        parentRoot->left = newNode;
     } else{
         // assign it to right tree
-        parentRoot->right = newRoot;
+        parentRoot->right = newNode;
     }
 
-    return root;
+    tree->size++;
+    return tree;
 }
 
 /*Search*/
-void* bst_find(const BinarySearchTree *root,const void *data,BSTCompareFunc compare){
-    const BinarySearchTree *currentRoot = root;
-    while (currentRoot != NULL){
-        int comparison = compare(data,currentRoot->data);
+void* bst_find(const BinarySearchTree *tree,const void *data,BSTCompareFunc compare){
+    if (tree == NULL || compare == NULL){
+        return NULL;
+    }
+    const BinarySearchTreeNode *currentNode = tree->root;
+    while (currentNode != NULL){
+        int comparison = compare(data,currentNode->data);
         if (comparison < 0){
             // if less than 0 go to left tree
-            currentRoot = currentRoot->left;
+            currentNode = currentNode->left;
         } else if (comparison > 0){
             // if greater than 0 go to right tree
-            currentRoot = currentRoot->right;
+            currentNode = currentNode->right;
         }else if (comparison == 0){
             // found the value
-            return currentRoot->data; 
+            return currentNode->data; 
         }
     }
     // if not found return NULL
@@ -74,21 +84,38 @@ void* bst_find(const BinarySearchTree *root,const void *data,BSTCompareFunc comp
 }
 
 /*Remove*/
-void* bst_remove(BinarySearchTree *root,const void *data,BSTCompareFunc compare);
+void* bst_remove(BinarySearchTree *tree,const void *data,BSTCompareFunc compare){
+}
 
 
 /*Information*/
-void* bst_size(const BinarySearchTree *root);
-
-void* bst_height(const BinarySearchTree *root);
+static inline int bst_max_int(const int first,const int second){
+    return (first > second) ? first : second;
+}
+static int bst_node_height(const BinarySearchTreeNode *node){
+    if (node == NULL){
+        return 0;
+    }
+    return 1 + bst_max_int(bst_node_height(node->left),bst_node_height(node->right));
+}
+// height(null) = 0 / height(leaf) = 1
+int bst_height(const BinarySearchTree *tree){
+    if (tree == NULL){
+        return 0;
+    }
+    return bst_node_height(tree->root);
+}
+size_t bst_size(const BinarySearchTree *tree){
+    return (tree == NULL) ? 0 : tree->size;
+}
 
 /*Traversal*/
 
-void bst_inorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_inorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
-void bst_preorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_preorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
-void bst_postorder(const BinarySearchTree *root, void (*visit)(const void *data));
+void bst_postorder(const BinarySearchTree *tree, void (*visit)(const void *data));
 
 /*Cleanup*/
-void bst_destroy(const BinarySearchTree *root, void (*destroyData)(void *data));
+void bst_destroy(const BinarySearchTree *tree, void (*destroyData)(void *data));
